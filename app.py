04217cbbfd14e381ac9cfc1bbdd355021c95b814
Adrien HUMMEL <132281657+Vortax1247@users.noreply.github.com>
2023-05-08 15:55:53 +0200

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__,template_folder='/home/vortax/test-python/flask-ML')
+app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
